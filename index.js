895a3dea99385545a92216c9566e5215918c2158
Anuratan1421 <123433209+Anuratan1421@users.noreply.github.com>
2025-04-17 12:52:17 +0530

    8 index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const nodemailer = require('nodemailer');
 const axios = require('axios');
 
 const app = express();
-const port = 3000;
+const port = 8000;
 const cors = require('cors');
 
 const http = require('http').createServer(app);
